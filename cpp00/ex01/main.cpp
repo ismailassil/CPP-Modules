41cb3e84d42048b3e7c5dae3e90f9931353903a4
Ismail ASSIL <61088999+ismailassil@users.noreply.github.com>
2024-05-15 22:27:49 +0000

    begin initializing the class (ADD)

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -11,31 +11,40 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include "class.hpp"
 /*
 first name, last name, nickname, phone number, and
 darkest secret.
 */
 int	main( void )
 {
-	using namespace	std;
-	string			str;
+	std::string	str[6];
+	PhoneBook	phonebook;
 
-	while (!cin.eof())
+	while (!std::cin.eof())
 	{
-		cout << "Enter a command: ";
-		getline(cin, str);
-		if (str == "ADD")
+		std::cout << "Enter a command: ";
+		std::getline(std::cin, str[0]);
+		if (str[0] == "ADD")
 		{
-			// add contact
+			std::cout << "Please, Fill all the information below" << std::endl;
+			std::cout << "First Name: ";
+			std::cin >> str[1];
+			std::cout << "Last Name: ";
+			std::cin >> str[2];
+			std::cout << "Nickname Name: ";
+			std::cin >> str[3];
+			std::cout << "Phone Number: ";
+			std::cin >> str[4];
+			std::cout << "Darkest Secret: ";
+			std::cin >> str[5];
+			phonebook.add(str);
 		}
-		else if (str == "SEARCH")
-			cout << "SEARCH\n";
-		else if (str == "EXIT")
+		else if (str[0] == "SEARCH")
 		{
-			cout << "EXIT";
+		}
+		else if (str[0] == "EXIT")
 			break ;
-		}
 	}
-	cout << "\n";
 	return (0);
 }
