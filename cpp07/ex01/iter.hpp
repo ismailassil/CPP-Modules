828ef72f4c58443765f6b6357a98f7b18d909c15
Ismail Assil <iassil@e3r3p18.1337.ma>
2024-10-23 15:43:53 +0100

    fixing segf in templates

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: iassil <iassil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 07:31:45 by iassil            #+#    #+#             */
-/*   Updated: 2024/10/14 11:38:44 by iassil           ###   ########.fr       */
+/*   Updated: 2024/10/23 15:41:43 by iassil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 template <typename T, typename F>
 void	iter( T& arr, size_t n, F func ) {
 	
+	if ( !arr )
+		return ;
+
 	for ( size_t i = 0; i < n; i++ ) {
 		func(arr[i]);
 	}
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,10 +6,11 @@
 /*   By: iassil <iassil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 07:34:29 by iassil            #+#    #+#             */
-/*   Updated: 2024/10/14 11:42:43 by iassil           ###   ########.fr       */
+/*   Updated: 2024/10/23 15:41:22 by iassil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cstddef>
 #include <iostream>
 #include "iter.hpp"
 
@@ -21,11 +22,15 @@ void print( T& c ) {
 int main() {
 	int tab[] = { 0, 1, 2, 3, 4 };
 	int tab2[12] = { 32, 99, 12, 87, 13, 66, 32, 84, 9, 102, 88, 1002 };
+	int *tabNULL = NULL;
 
 	iter( tab, 5, print<int> );
 	std::cout << std::endl;
 
 	iter( tab2, 12, print<int> );
+	std::cout << std::endl;
+
+	iter( tabNULL, 12, print<int> );
 	std::cout << std::endl;
 
 	return ( 0 );
